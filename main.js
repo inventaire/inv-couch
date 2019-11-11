51bb49cd0b69dfa2a1d81a3048acf382d93bae4c
maxlath <k@maxlath.eu>
2019-11-11 23:58:59 +0100

    lint
    and also fix couch_.ignoreNotFound which wasn't rethrowing errors as expected

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-let couch_;
-module.exports = (couch_ = {});
+const couch_ = module.exports = {}
 
-couch_.mapResult = (res, type) => res.rows.map(row => row[type]);
-couch_.mapDoc = res => couch_.mapResult(res, 'doc');
-couch_.mapId = res => res.rows.map(row => row.id);
-couch_.mapValue = res => res.rows.map(row => row.value);
-couch_.mapValueId = res => res.rows.map(row => row.value._id);
+couch_.mapResult = (res, type) => res.rows.map(row => row[type])
+couch_.mapDoc = res => couch_.mapResult(res, 'doc')
+couch_.mapId = res => res.rows.map(row => row.id)
+couch_.mapValue = res => res.rows.map(row => row.value)
+couch_.mapValueId = res => res.rows.map(row => row.value._id)
 
-couch_.firstDoc = docs => docs != null ? docs[0] : undefined;
+couch_.firstDoc = docs => docs != null ? docs[0] : null
 
-couch_.joinOrderedIds = function(idA, idB){
-  if (idA < idB) { return `${idA}:${idB}`;
-  } else { return `${idB}:${idA}`; }
-};
+couch_.joinOrderedIds = function (idA, idB) {
+  if (idA < idB) return `${idA}:${idB}`
+  else return `${idB}:${idA}`
+}
 
-couch_.ignoreNotFound = function(err){
-  if ((err != null ? err.error : undefined) === 'not_found') { return; }
-};
+couch_.ignoreNotFound = err => {
+  if (!(err && err.error === 'not_found')) throw err
+}
 
-couch_.getObjIfSuccess = function(db, body){
+couch_.getObjIfSuccess = (db, body) => {
   if ((db.get != null) && body.ok) {
-    return db.get(body.id);
+    return db.get(body.id)
   } else if (db.get != null) {
-    throw new Error(`${body.error}: ${body.reason}`);
+    throw new Error(`${body.error}: ${body.reason}`)
   } else {
-    throw new Error("bad db object passed to _.getObjIfSuccess");
+    throw new Error('bad db object passed to _.getObjIfSuccess')
   }
-};
+}
