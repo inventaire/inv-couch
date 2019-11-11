05bd08bcf81ab6d6b58e2f4fccd757c714816c99
decaffeinate <jums@inventaire.io>
2019-11-11 20:47:34 +0100

    decaffeinate: Convert main.coffee to JS

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-module.exports = couch_ = {}
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+let couch_;
+module.exports = (couch_ = {});
 
-couch_.mapResult = (res, type)-> res.rows.map (row)-> row[type]
-couch_.mapDoc = (res)-> couch_.mapResult res, 'doc'
-couch_.mapId = (res)-> res.rows.map (row)-> row.id
-couch_.mapValue = (res)-> res.rows.map (row)-> row.value
-couch_.mapValueId = (res)-> res.rows.map (row)-> row.value._id
+couch_.mapResult = (res, type) => res.rows.map(row => row[type]);
+couch_.mapDoc = res => couch_.mapResult(res, 'doc');
+couch_.mapId = res => res.rows.map(row => row.id);
+couch_.mapValue = res => res.rows.map(row => row.value);
+couch_.mapValueId = res => res.rows.map(row => row.value._id);
 
-couch_.firstDoc = (docs)-> docs?[0]
+couch_.firstDoc = docs => docs != null ? docs[0] : undefined;
 
-couch_.joinOrderedIds = (idA, idB)->
-  if idA < idB then "#{idA}:#{idB}"
-  else "#{idB}:#{idA}"
+couch_.joinOrderedIds = function(idA, idB){
+  if (idA < idB) { return `${idA}:${idB}`;
+  } else { return `${idB}:${idA}`; }
+};
 
-couch_.ignoreNotFound = (err)->
-  if err?.error is 'not_found' then return
+couch_.ignoreNotFound = function(err){
+  if ((err != null ? err.error : undefined) === 'not_found') { return; }
+};
 
-couch_.getObjIfSuccess = (db, body)->
-  if db.get? and body.ok
-    return db.get(body.id)
-  else if db.get?
-    throw new Error "#{body.error}: #{body.reason}"
-  else
-    throw new Error "bad db object passed to _.getObjIfSuccess"
+couch_.getObjIfSuccess = function(db, body){
+  if ((db.get != null) && body.ok) {
+    return db.get(body.id);
+  } else if (db.get != null) {
+    throw new Error(`${body.error}: ${body.reason}`);
+  } else {
+    throw new Error("bad db object passed to _.getObjIfSuccess");
+  }
+};
